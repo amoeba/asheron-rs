a1269b814f58634487b2e6e7be0f7ad04f6bd1a7
Bryce Mecum <petridish@gmail.com>
2025-12-01 19:25:34 -0800

    WIP: Get types with switches parsing

--- crates/genlib/src/lib.rs
+++ crates/genlib/src/lib.rs
@@ -1,3 +1,4 @@
+use core::panic;
 use std::collections::HashMap;
 
 use log::debug;
@@ -12,7 +13,7 @@ struct Field {
 #[derive(Debug)]
 struct VariantFieldSet {
     name: String,
-    fields: HashMap<u32, VariantFieldSet>,
+    fields: HashMap<String, Vec<Field>>,
 }
 
 #[derive(Debug)]
@@ -68,8 +69,6 @@ fn safe_field_name(name: &str) -> (String, bool) {
 }
 
 fn generate_type(schema_type: &SchemaType) -> String {
-    env_logger::init();
-
     let type_name = &schema_type.name;
     println!("generate_type: name = {type_name}");
 
@@ -157,34 +156,52 @@ pub fn generate(xml: &str, filter_types: Option<Vec<String>>) -> String {
                         debug!("current_type is now {current_type:?}");
                     }
                 } else if tag_name == "field" {
-                    if let Some(ref mut ty) = current_type {
-                        let mut field_type = None;
-                        let mut field_name = None;
-                        let mut field_text = None;
+                    let mut field_type = None;
+                    let mut field_name = None;
+                    let mut field_text = None;
 
-                        for attr in e.attributes().flatten() {
-                            match attr.key.as_ref() {
-                                b"type" => {
-                                    field_type = Some(attr.unescape_value().unwrap().into_owned())
-                                }
-                                b"name" => {
-                                    field_name = Some(attr.unescape_value().unwrap().into_owned())
-                                }
-                                b"text" => {
-                                    field_text = Some(attr.unescape_value().unwrap().into_owned());
-                                }
-                                _ => {}
+                    for attr in e.attributes().flatten() {
+                        match attr.key.as_ref() {
+                            b"type" => {
+                                field_type = Some(attr.unescape_value().unwrap().into_owned())
+                            }
+                            b"name" => {
+                                field_name = Some(attr.unescape_value().unwrap().into_owned())
+                            }
+                            b"text" => {
+                                field_text = Some(attr.unescape_value().unwrap().into_owned());
+                            }
+                            _ => {}
+                        }
+                    }
+
+                    debug!("Processing field {field_name:?}");
+
+                    if let (Some(fname), Some(ftype)) = (field_name, field_type) {
+                        // TODO: Add to current field variant if we're in one
+                        if let Some(ref mut var) = current_variant_fieldset {
+                            debug!("Processing field as variant field...");
+                            let new_field = Field {
+                                name: fname,
+                                field_type: ftype,
+                            };
+
+                            if let Some(ref id) = current_variant_field_id {
+                                let variant_fieldset =
+                                    var.fields.entry(id.to_string()).or_insert(Vec::new());
+                                variant_fieldset.push(new_field);
                             }
                         }
-
-                        if let (Some(fname), Some(ftype)) = (field_name, field_type) {
+                        // Normal field behavior below
+                        else if let Some(ref mut ty) = current_type {
+                            debug!("Processing field as normal field...");
                             ty.fields.push(Field {
                                 name: fname,
                                 field_type: ftype,
                             });
-                        }
 
-                        debug!("after adding fields, current_type is now {current_type:?}");
+                            debug!("after adding fields, current_type is now {ty:?}");
+                        }
                     }
                 } else if tag_name == "switch" {
                     let mut name: Option<String> = None;
@@ -195,7 +212,7 @@ pub fn generate(xml: &str, filter_types: Option<Vec<String>>) -> String {
                         }
                     }
 
-                    // Mark current type as variant so we start accumulating fields as variants
+                    // Set a variant so we accumulate fields into the variant for now
                     if let Some(name) = name {
                         current_variant_fieldset = Some(VariantFieldSet {
                             name,
@@ -208,24 +225,29 @@ pub fn generate(xml: &str, filter_types: Option<Vec<String>>) -> String {
                     let mut value = None;
 
                     for attr in e.attributes().flatten() {
-                        match attr.key.as_ref() {
-                            b"value" => value = Some(attr.unescape_value().unwrap().into_owned()),
-                            _ => {}
+                        if attr.key.as_ref() == b"value" {
+                            value = Some(attr.unescape_value().unwrap().into_owned())
                         }
                     }
 
                     debug!("TODO: Need to handle <case> element");
                     current_variant_field_id = value;
                     debug!("current_variant_field_id is now {current_variant_field_id:?}");
+                    // if let (Some(val), Some(ref mut current)) = (value, current_variant_fieldset) {
+                    //     current.fields.insert(val, Vec::new());
+                    // }
                 }
             }
             Ok(Event::End(e)) => {
                 // closing </type>
                 if e.name().as_ref() == b"type" {
+                    // Close out type
                     if let Some(ty) = current_type.take() {
                         types.push(ty);
+                        debug!("DONE: {types:?}");
                     }
                 } else if e.name().as_ref() == b"switch" {
+                    // Set current variant on current type and reset current_variant_fieldset
                     if let Some(ref mut ty) = current_type {
                         if let Some(v) = current_variant_fieldset.take() {
                             ty.variant = Some(v);
